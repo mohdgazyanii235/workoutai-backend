7aed86daa663479e7486c27e4289966871a5f8c2
Mohamed Yusuf <mohdgazyanii235@gmail.com>
2025-10-17 20:20:43 +0100

    database url fro menv

--- app/database.py
+++ app/database.py
@@ -2,8 +2,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@127.0.0.1:5432/workout_ai_dev"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
